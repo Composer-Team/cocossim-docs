4c8028695f3fc288d3396a974ac7f005209c5396
mc186 <115806113+mc186@users.noreply.github.com>
2025-09-26 11:52:08 -0400

    Updates

---
+++ assets/js/5e95c892.5ab64e0c.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkcocossim_docs=self.webpackChunkcocossim_docs||[]).push([[647],{7121:(s,e,c)=>{c.r(e),c.d(e,{default:()=>t});c(6540);var r=c(4164),o=c(7559),u=c(5500),a=c(2831),d=c(1656),n=c(4848);function t(s){return(0,n.jsx)(u.e3,{className:(0,r.A)(o.G.wrapper.docsPages),children:(0,n.jsx)(d.A,{children:(0,a.v)(s.route.routes)})})}}}]);
