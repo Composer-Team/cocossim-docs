5cb011a5e7451afac51e19974f094c1a1efedda2
mc186 <115806113+mc186@users.noreply.github.com>
2025-09-26 12:13:02 -0400

    Fix baseUrl configuration for GitHub Pages

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,12 +18,12 @@ const config: Config = {
   url: 'https://composer-team.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/cocossim/',
+  baseUrl: '/cocossim-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'composer-team', // Usually your GitHub org/user name.
-  projectName: 'cocossim', // Usually your repo name.
+  projectName: 'cocossim-docs', // Usually your repo name.
 
   onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
